3cf9a2a9992c0d41be0c4c9e8cb686ff51de2802
Rorgrlrio <roger.rs.04.07.02@gmail.com>
2022-11-07 10:03:17 -0600

    Haciendo la funcion abrirSD

--- app/src/main/java/com/example/myapplication/ArchivoActivity.java
+++ app/src/main/java/com/example/myapplication/ArchivoActivity.java
@@ -2,13 +2,107 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.Guard;
 
 public class ArchivoActivity extends AppCompatActivity {
+    
+    //Objetos para asociar los componentes
+    EditText nombreArchivo;
+    Button accion;
+    
+    //Variables para identificar accion y contenido a guardar en archivo
+    String contenido = "";
+    int tipo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archivo);
-    }
+        
+        //Asociar objetos con componenetes
+        nombreArchivo = (EditText) findViewById(R.id.txtNombreArchivo);
+        accion = (Button) findViewById(R.id.btnAccion);
+        
+        //Se identifica el tipo de accion 1-Abrir 2-Guardar por default 1
+        tipo = (int) getIntent().getIntExtra("tipo", 1);
+        
+        //Se cambio el nombre del boton
+        switch(tipo){
+            case 1: accion.setText("Abrir"); break;
+            case 2: accion.setText("Guardar");
+                //Se obtine la informacion a guardar
+                contenido = (String) getIntent().getStringExtra("contenido");
+                break;
+        }//switch
+        
+    }//onCreate
+
+    public void realizaAccion(View view){
+        //Se obtiene lo escrito en el cuadro de texto
+        String nomArch = nombreArchivo.getText().toString();
+
+        //Se valida que no este vacio
+        if(nomArch != ""){
+            //De acuerdo al tipo de accion se ejecuta el método correspondiente
+            switch(tipo){
+                case 1: abrirArchivoSD(nomArch); break;
+                case 2: guardarArchivoSD(nomArch); break;
+            }//switch
+            finish();
+
+        }else{
+            Toast.makeText(this, "Debes escribir un nombre", Toast.LENGTH_SHORT).show();
+        }
+    } //realizaAccion
+    
+    private void abrirArchivoSD(String n){
+        try{
+            //Se obtiene la ruta de almcaenamiento externo
+            File tarjetaSD = Environment.getExternalStorageDirectory();
+            //Se define el nombre del archivo y la ruta donde sera guardado
+            File archivo = new File(tarjetaSD.getPath(), n);
+            //Abrir el archivo
+            InputStreamReader abrirArchivo = new InputStreamReader(openFileInput(archivo.getName()));
+            //Asocia el flujo con el archivo para obtener la informacion
+            BufferedReader leerArchivo = new BufferedReader(abrirArchivo);
+            //Lectura del archivo y colocar en una variable de tipo cadena
+            String linea = leerArchivo.readLine();
+            //Variable que guarddara la informacion leida desde el archivo, se inicializa en vacio
+            String textoLeido = "";
+            
+            //Ciclo para leer el contenido del archivo
+            while (linea !=null){
+                textoLeido += linea + '\n';
+                linea = leerArchivo.readLine();
+            }//while
+            
+            //Cerrar el fluijo del archivo
+            leerArchivo.close();
+            //Cerrar el archivo
+            abrirArchivo.close();
+            
+            //Objeto para conectar a otra activity para guardar archivo
+            Intent intent = new Intent(this,MainActivity.class);
+            intent.putExtra("contenido", textoLeido);
+            
+            //Iniciar la activity
+            startActivity(intent);
+            
+        }catch (IOException e){
+            Toast.makeText(this, "El archivo no se pudo leer", Toast.LENGTH_SHORT).show();
+        }
+    }//abrirArchivoSD
+    
 }
